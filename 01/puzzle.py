5e958c603d165125db8ee1664d6abb5d7c9d1440
Damon <mdamon.connolly@gmail.com>
2019-08-09 10:42:14 +0100

    Minor Changes
    Cleaned up certain aspects of creation. Formatted the output
    Added a main function.

--- 01/puzzle.py
+++ 01/puzzle.py
@@ -10,43 +10,40 @@ class Puzzler():
         self.answer = 0
         self.rowsum = 0
 
-        random.seed(50)
+        random.seed(random.randint(1, 100))
 
         for i in range(0, 7):
             self.columns.append(self.generateColumn())
-
-
-        print(self.columns)
+        self.rowsum = self.answer
         self.shuffle()
-        print(self.columns)
-
 
 
     def generateColumn(self):
         column = deque([])
         for i in range(0, 5):           
-            column.append(random.randint(0, 9))
-
+            column.append(random.randint(1, 9))
+        
         self.answer += column[2]
-
         return column
 
 
     def shuffle(self):
-        
-        for column in self.columns:
-            column.rotate(random.randint(-20, 20))
+        while self.rowsum == self.answer:
+            for column in self.columns:
+                column.rotate(random.randint(-20, 20))
+
+            self.updateRowSum()
 
 
     def move(self, col, dir):
-        
         if dir == '+':
             self.columns[col].rotate(-1)
         elif dir == '-':
             self.columns[col].rotate(1)
 
-    def draw(self):
+        self.updateRowSum()
 
+    def draw(self):
         '''Clear the screen first'''
         if name == 'nt': 
             system('cls') 
@@ -56,25 +53,46 @@ class Puzzler():
         print('========================================================')
         print('                      YOUR GOAL IS {0}                  '.format(self.answer))
         print('========================================================')
+        print('[0] - [1] - [2] - [3] - [4] - [5] - [6]')
+        print(' |     |     |     |     |     |     |')
+        print(' |     |     |     |     |     |     |')
+        print(' V     V     V     V     V     V     V')
         for i in range (0, 5):
             row = []
-            for j in self.columns:
-                row.append(j[i])
+            for column in self.columns:
+                row.append(column[i])
 
             if i is 2:
                 self.rowsum = sum(row)
-                print('[{0}] - [{1}] - [{2}] - [{3}] - [{4}] - [{5}] - [{6}]  == [{7}]'.format(row[0], row[1], row[2], row[3], row[4], row[5], row[6], sum(row)))
+                print('[{0}] - [{1}] - [{2}] - [{3}] - [{4}] - [{5}] - [{6}]  == [{7}]'.format(row[0], row[1], row[2], row[3], row[4], row[5], row[6], self.rowsum))
             else:
                 print('[{0}] - [{1}] - [{2}] - [{3}] - [{4}] - [{5}] - [{6}]'.format(row[0], row[1], row[2], row[3], row[4], row[5], row[6]))
-
         print('========================================================')
 
+
+    def updateRowSum(self):
+        row = 0
+        for column in self.columns:
+            self.rowsum += column[2]
+
     def puzzleLoop(self):
-        
-        while self.answer != self.rowsum:
 
+        complete = False
+        while not complete:
             self.draw()
-            
-            userIn = input('Move a column with:  columnIndex, +/-\n')
 
-            self.move(userIn.split(',')[0], userIn.split(',')[1])
+            '''TODO: Handle user input errors'''
+            userIn = input('Move a column with:  columnIndex+/-\nExample: 3+\n')
+
+            self.move(int(userIn[0]), userIn[1])
+
+            if self.rowsum == self.answer:
+                complete == True
+
+        print("You Solved the puzzle. Yay.")
+
+
+if __name__ == '__main__':
+
+    p = Puzzler()
+    p.puzzleLoop()
