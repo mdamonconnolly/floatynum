9620b540cee4df226532d4200de9b1531fb59325
Damon <mdamon.connolly@gmail.com>
2019-09-27 19:30:00 +0100

    Add Blipping.

--- b_soundbytes/src/main.rs
+++ b_soundbytes/src/main.rs
@@ -1,12 +1,12 @@
 
 
 //Prints it out as a graph.
-fn update_display(_byte_stream:[i16;64])
+fn update_display(byte_stream:&[i16;64])
 {
-    for i in (0..10).rev() {
+    for i in (0..13).rev() {
         for j in 0..63 {
-            if _byte_stream[j] == i {
-                print!(" {0} ", _byte_stream[j]);
+            if byte_stream[j] == i {
+                print!(" {0} ", byte_stream[j]);
             }
             else {
                 print!(" · ");
@@ -16,12 +16,35 @@ fn update_display(_byte_stream:[i16;64])
     }
 }
 
+/*
+fn compressor_node(threshold:i16, ratio:i16, mut &_byte_stream:[i16;64])
+{
+    for i in 0..63
+    {
+        _byte_stream[i] = ((_byte_stream[i] - threshold) / ratio) + threshold;
+    }
+}
+*/
+
+fn blip(position:usize, magnitude:i16, Q:i16, mut byte_stream:&mut [i16;64])
+{
+    byte_stream[position] = magnitude;
+    calculate_falloff(position, Q);
+
+}
+
+fn calculate_falloff(_position:usize, _Q:i16)
+{
+
+}
+
 //Main function with array stream.
 fn main() {
     
     //Test cases can change here.
     let mut stream:[i16;64] = [0;64];
 
+    blip(22, 5, 3, &mut stream);
 
-    update_display(stream);
+    update_display(&stream);
 }
