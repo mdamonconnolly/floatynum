9cb71a51dce3b7bc0d7adee9b7cff1917535059e
Damon <mdamon.connolly@gmail.com>
2019-09-27 13:49:26 +0100

    Added print formatting.

--- b_soundbytes/src/main.rs
+++ b_soundbytes/src/main.rs
@@ -1,3 +1,27 @@
+
+
+//Prints it out as a graph.
+fn update_display(_byte_stream:[i16;64])
+{
+    for i in (0..10).rev() {
+        for j in 0..63 {
+            if _byte_stream[j] == i {
+                print!(" {0} ", _byte_stream[j]);
+            }
+            else {
+                print!(" · ");
+            }
+        }
+        print!("\n");
+    }
+}
+
+//Main function with array stream.
 fn main() {
-    println!("Hello, world!");
+    
+    //Test cases can change here.
+    let mut stream:[i16;64] = [0;64];
+
+
+    update_display(stream);
 }
