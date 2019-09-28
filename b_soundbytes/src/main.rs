388417e313863daf765750b1a6f19ff2170f30b6
Damon <mdamon.connolly@gmail.com>
2019-09-28 10:58:50 +0100

    small update

--- b_soundbytes/src/main.rs
+++ b_soundbytes/src/main.rs
@@ -6,7 +6,7 @@ fn update_display(byte_stream:&[i16;64])
     for i in (0..13).rev() {
         for j in 0..63 {
             if byte_stream[j] == i {
-                print!(" {0} ", byte_stream[j]);
+                print!(" X ");
             }
             else {
                 print!(" · ");
@@ -21,17 +21,20 @@ fn compressor_node(threshold:i16, ratio:i16, mut &_byte_stream:[i16;64])
 {
     for i in 0..63
     {
-        _byte_stream[i] = ((_byte_stream[i] - threshold) / ratio) + threshold;
+        _byte_stream[i] = ((_byte_stream[i] - threshold)/ratio) + threshold;
     }
 }
 */
 
-fn blip(position:usize, magnitude:i16, Q:i16, mut byte_stream:&mut [i16;64])
+fn blip(position:usize, magnitude:i16, byte_stream:&mut [i16;64])
 {
     byte_stream[position] = magnitude;
-    calculate_falloff(position, Q, byte_stream);
 
     //Calculate Falloff
+    for i in 0..5{
+        byte_stream[(position + i)] = magnitude/i16::from(i);
+        byte_stream[(position - i)] = magnitude/i16::from(i);
+    }
 }
 
 //Main function with array stream.
@@ -40,7 +43,7 @@ fn main() {
     //Test cases can change here.
     let mut stream:[i16;64] = [0;64];
 
-    blip(22, 5, 3, &mut stream);
+    blip(22, 5, &mut stream);
 
     update_display(&stream);
 }
